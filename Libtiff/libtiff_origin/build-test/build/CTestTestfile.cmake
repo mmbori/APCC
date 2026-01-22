cd39a348286079f379a1e57367bc39797b67dd5e
mmbori <tyzhffkaos75@gmail.com>
2026-01-23 07:07:19 +0900

    Add some missing files to libtiff directory

---
+++ Libtiff/libtiff_origin/build-test/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bhsu/libtiff_origin/build
+# Build directory: /home/bhsu/libtiff_origin/build-test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Libtiff/libtiff_transformed/build-prog/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bhsu/libtiff_tmp/build
+# Build directory: /home/bhsu/libtiff_tmp/build-prog/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
